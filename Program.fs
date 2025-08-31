d1b9cb5d16767d7b2150293de573e2e73d13b925
gigi <5151947+gigibytes@users.noreply.github.com>
2025-08-30 21:17:06 -0400

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,49 @@
+﻿open System
+
+// maybe this should be System_clipboard with entry and history submodules
+module Clipboard = 
+    module Entry =
+        type t = string
+
+    module History =
+        let empty : Entry.t list = []
+        let get_current_clipboard_content =
+            let is_text = Windows.Forms.Clipboard.ContainsText() in
+            match is_text with
+            | true -> Some (Windows.Forms.Clipboard.GetText())
+            | false -> None
+
+        let add history = 
+            let entry_content = get_current_clipboard_content
+            in
+            match entry_content with
+            | Some text -> List.append history [Entry.t text]
+            | None -> history
+        
+        let latest_entry history = List.last history
+        let new_entry_detected history =
+            let current_clipboard_content = get_current_clipboard_content
+            in
+            match current_clipboard_content with
+            // why is fsharp like this lol. List.append can take multiple args w/o parens,
+            // but String.Equals refuses to compile unless in parens
+            // None may mean there is data that is not text on the clipboard
+            | None -> false
+            // May want to make this not detect a new entry if the data on clipboard is in the last n (say, 10)
+            // history entries
+            | Some c when String.Equals(latest_entry history, c) -> false
+            | _ -> true
+
+
+// Poll for new data
+
+// someday: support adding a permanent entry and dynamic entries (like formatting the date into an entry)
+
+[<EntryPoint; STAThread>]
+let main argv =
+    let clipboard_history= Clipboard.History.add Clipboard.History.empty
+    // TODO run this in a loop -- listen until quit and print new entries with ordered history as they happen
+    printfn "This is what was last copied to the clipboard: %s" (Clipboard.History.latest_entry clipboard_history)
+    printfn "This is the clipboard history"
+    // TODO once i have a 'poc' working under the hood as i want it to, add a gui
+    0 // return an integer exit code
